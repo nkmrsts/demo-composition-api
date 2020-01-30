274b7476d181da58d3783e4806d8835c2a4e0ec1
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-01-30 11:59:59 +0900

    雛形の作成

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { createComponent } from '@vue/composition-api'
 
-export default Vue.extend({
-  name: "app",
-  components: {
-    HelloWorld
-  }
-});
+export default createComponent({
+  setup() {}
+})
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
