a29910a8673999763d9758234d30440bad2807d1
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-01-31 11:14:28 +0900

    eslintrcを編集

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,6 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
   rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    eqeqeq: 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': ['error', { before: true, after: true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
--- README.md
+++ README.md
@@ -27,7 +27,7 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 yarn server
 
 
-### setting.json
+### VS Code設定 setting.json
 .vscode/setting.json
 ```
 {
